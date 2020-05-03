bfc1550d700284a5bbb5c153a1e0988a098bd56a
Qazi Amaan <qazi9amaan@gmail.com>
2020-05-03 08:34:02 +0530

    Instgram CLone Laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row ml-2">
+        <div class="col-3 p-4 ">
+            <img src="https://instagram.fblr8-1.fna.fbcdn.net/v/t51.2885-19/s150x150/92951437_542692753346089_8597032588097355776_n.jpg?_nc_ht=instagram.fblr8-1.fna.fbcdn.net&_nc_ohc=aFpLllSQwOoAX_ZldNT&oh=17fe25d4dd8c341242398f2fbdc1a7e6&oe=5ED67348"
+                 class="rounded-circle">
+        </div>
+        <div class="col-9 pt-4 ">
+            <div>
+                <h1>qaziamaanyousuf</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"> <strong>12</strong> Posts</div>
+                <div class="pr-5"> <strong>12</strong> Followers</div>
+                <div class="pr-5"> <strong>12</strong> Following</div>
+            </div>
+            <div class="pt-3 font-weight-bold">
+                Cause we're young and reckless❤️
+            </div>
+            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo tristique odio, eget ultricies justo viverra mattis. Donec in tortor non nibh auctor mattis vel in turpis. Aliquam dignissim dui quis Nunc.    </div>
+            <div><a href="#">www.kirangle.com</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://instagram.fblr8-1.fna.fbcdn.net/v/t51.2885-19/s150x150/92951437_542692753346089_8597032588097355776_n.jpg?_nc_ht=instagram.fblr8-1.fna.fbcdn.net&_nc_ohc=aFpLllSQwOoAX_ZldNT&oh=17fe25d4dd8c341242398f2fbdc1a7e6&oe=5ED67348" class="w-100 ">
+        </div>
+        <div class="col-4 ">
+            <img src="https://instagram.fblr8-1.fna.fbcdn.net/v/t51.2885-19/s150x150/92951437_542692753346089_8597032588097355776_n.jpg?_nc_ht=instagram.fblr8-1.fna.fbcdn.net&_nc_ohc=aFpLllSQwOoAX_ZldNT&oh=17fe25d4dd8c341242398f2fbdc1a7e6&oe=5ED67348" class="w-100 ">
+        </div>
+        <div class="col-4 ">
+            <img src="https://instagram.fblr8-1.fna.fbcdn.net/v/t51.2885-19/s150x150/92951437_542692753346089_8597032588097355776_n.jpg?_nc_ht=instagram.fblr8-1.fna.fbcdn.net&_nc_ohc=aFpLllSQwOoAX_ZldNT&oh=17fe25d4dd8c341242398f2fbdc1a7e6&oe=5ED67348" class="w-100 ">
+        </div>
+    </div>
+
+</div>
+@endsection
